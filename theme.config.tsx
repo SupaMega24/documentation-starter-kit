0685d64c9914cef04ff877675fd3a76cc2977424
SupaMega24 <138974329+SupaMega24@users.noreply.github.com>
2024-08-04 21:32:35 +0900

    Update pages

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: '© RunBro | All Rights Reserved | Nextra Docs Template'
   },
 }
 
